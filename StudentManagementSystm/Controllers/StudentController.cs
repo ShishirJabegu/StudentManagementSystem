6078cdf767928d1a5623a5aadb2e24ec45ce4f3a
ShishirJabegu <shishirjabegu@gmail.com>
2024-05-19 16:55:44 +0545

    action added

--- StudentManagementSystm/Controllers/StudentController.cs
+++ StudentManagementSystm/Controllers/StudentController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using StudentManagementSystem.Data;
 using StudentManagementSystem.Models;
+using StudentManagementSystem.Repository.Interface;
 using StudentManagementSystem.ViewModel;
 using System;
 using System.Runtime.CompilerServices;
@@ -11,37 +13,35 @@ namespace StudentManagementSystm.Controllers
     public class StudentController : Controller
 
     {
+        private readonly IStudentRepository _studentRepo;
 
-        private readonly ApplicationDbContext _context;
-        public StudentController(ApplicationDbContext context)
+        public StudentController(IStudentRepository studentRepo)
         {
-              _context = context;
+            
+            _studentRepo= studentRepo;
         }
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Students()
         {
-            var students= _context.StudentTable.ToList();
+            var students= _studentRepo.GetAll();
             return View(students);
         }
-
         [HttpGet]
         public IActionResult AddStudent()
         {
-            return View();
-       
- }
+            return View();      
+        }
         [HttpPost]
         public IActionResult AddStudent(StudentVM vm)
         {
             if (!ModelState.IsValid)
             {
+                _studentRepo.Notify("Failed to Registered!!");
                 return View(vm);
             }
-
             var studentvm = new Student()
             {
                 Name = vm.Name, 
@@ -52,18 +52,18 @@ namespace StudentManagementSystm.Controllers
                Address= vm.Address,
 
             };
-             _context.StudentTable.Add(studentvm);
-            _context.SaveChanges();
+            _studentRepo.Add(studentvm);
+            _studentRepo.Save();
+            _studentRepo.Notify("Successfully Created");
             return RedirectToAction("Students");
         }
-
         [HttpGet]
         public IActionResult EditStudent(int id)
         {
-            var studentuser = _context.StudentTable.FirstOrDefault (x =>x.Id == id);
+            var studentuser = _studentRepo.Get (x =>x.Id == id);
             if (studentuser != null)
             {
-                var std = new UpdateVM()
+                var std = new UpdateVM
                 {
                     Name = studentuser.Name,
                     Grade = studentuser.Grade,
@@ -76,17 +76,16 @@ namespace StudentManagementSystm.Controllers
                 return View(std);
             }
             return RedirectToAction("Students");
-
         }
-
         [HttpPost]
         public IActionResult EditStudent(UpdateVM vm)
         {
            if(!ModelState.IsValid)
             {
+                _studentRepo.Notify("Failed to Update");
                 return View(vm);
             }
-           var student = _context.StudentTable.Find(vm.Id);
+           var student = _studentRepo.FindById(vm.Id);
            if (student!=null) {
                 student.Name = vm.Name;
                 student.Grade = vm.Grade;
@@ -94,34 +93,30 @@ namespace StudentManagementSystm.Controllers
                 student.Sex = vm.Sex;
                 student.Faculty = vm.Faculty;
                 student.RollNo = vm.RollNo;
-
-           _context.SaveChanges();
-            return RedirectToAction("Students");
+                _studentRepo.Save();
+                _studentRepo.Notify("Successfully Updated");
+                return RedirectToAction("Students");
            }
             else{
                 return View(vm);
-            }
-
-           
-
+            }        
         }
+              
         public IActionResult Delete(int id)
         {
-           var student = _context.StudentTable.Find(id);
+           var student = _studentRepo.FindById(id);
             if (student!=null)
             {
-                _context.StudentTable.Remove(student);
-            _context.SaveChanges();
-            return RedirectToAction("Students");
-            }
-            return View("ERROR");
-            
+                _studentRepo.Remove(student);
+                _studentRepo.Save();
+                _studentRepo.Notify("Successfully Deleted");
+                return RedirectToAction("Students");
+            }           
+            return View("ERROR");          
         }
-
         public IActionResult ERROR()
         {
             return View();
         }
-
     }
 }
